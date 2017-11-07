ed725b32e5ad0792aac46f8ef0c6ab7348b3497c
NikitaLipatov <Ovenvan@yandex.ru>
2017-11-07 19:57:36 +0300

    Важный коммит

--- src/main/java/cardgame/controller/UserController.java
+++ src/main/java/cardgame/controller/UserController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String registrationUser(@RequestBody String request) {
-        System.err.println(request);
+    public String registrationUser(@RequestBody Map<String, String> request) {
+        System.err.println(request.get("field"));
         return "test";
     }
 }
