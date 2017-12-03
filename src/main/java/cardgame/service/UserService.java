5e5fa08f9aafccf0c17a3020b07a0337336152f8
NikitaLipatov <Ovenvan@yandex.ru>
2017-12-03 18:10:02 +0300

    фйфывфв

---
+++ src/main/java/cardgame/service/UserService.java
@@ -0,0 +1,7 @@
+package cardgame.service;
+
+/**
+ * Created by NikitaLipatov on 03.12.2017.
+ */
+public interface UserService {
+}
---
+++ src/main/java/cardgame/service/impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package cardgame.service.impl;
+
+/**
+ * Created by NikitaLipatov on 03.12.2017.
+ */
+public class UserServiceImpl {
+}
